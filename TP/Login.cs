b43e9ced64f501c455e81ab730d722f9cd7fbe0a
HMJda <hmj4669@gmail.com>
2022-11-26 22:39:36 +0900

    초기 커밋.

---
+++ TP/Login.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP
+{
+    public partial class Login : Form
+    {
+        public bool test = false;
+        TextBox[] txtList;
+        const string IdPlaceholder = "아이디";
+        const string PwPlaceholder = "비밀번호";
+
+        public Login()
+        {
+            InitializeComponent();
+
+            txtList = new TextBox[] { textBox1, textBox2 };
+            foreach (var txt in txtList)
+            {
+                txt.ForeColor = Color.DarkGray;
+                if (txt == textBox1)
+                    txt.Text = IdPlaceholder;
+                else if (txt == textBox2)
+                    txt.Text = PwPlaceholder;
+                txt.GotFocus += RemovePlaceholder;
+                txt.LostFocus += SetPlaceholder;
+            }
+        }
+        private void RemovePlaceholder(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            if (txt.Text == IdPlaceholder | txt.Text == PwPlaceholder)
+            {
+                txt.ForeColor = Color.Black;
+                txt.Text = String.Empty;
+                if (txt == textBox2)
+                    textBox2.PasswordChar = '●';
+            }
+        }
+        private void SetPlaceholder(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            if (string.IsNullOrEmpty(txt.Text))
+            {
+                txt.ForeColor = Color.DarkGray;
+                if (txt == textBox1)
+                    txt.Text = IdPlaceholder;
+                else if (txt == textBox2)
+                {
+                    txt.Text = PwPlaceholder;
+                    textBox2.PasswordChar = default;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ TP/Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess;
+using Oracle.ManagedDataAccess.Client;
+
+namespace TP
+{
+    public partial class Order : Form
+    {
+        public Order()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private string DB_Server_Info = "Data Source=localhost:1521/xe;" +
+            "User ID = system; Password = 1;";
+        private void Order_Load(object sender, EventArgs e)
+        {
+            Oracle.ManagedDataAccess.Client.OracleConnection conn = new Oracle.ManagedDataAccess.Client.OracleConnection(DB_Server_Info);
+            conn.Open();
+        }
+    }
+}
