e41d3c9728a60bd39e4f2dc4ef33a1e46b145af2
HMJda <hmj4669@gmail.com>
2022-12-02 19:53:03 +0900

    로그아웃 버튼 구현

--- TP/setting.cs
+++ TP/setting.cs
@@ -12,9 +12,23 @@ namespace TP
 {
     public partial class setting : Form
     {
-        public setting()
+        Main main;
+        public setting(Main main)
         {
+            this.main = main;
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {  //로그아웃 버튼
+            this.Visible = false;
+            Form form = new Login(main);      
+            form.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
