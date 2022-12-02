8f20a7d11fd96b5b8e5e02e5485d8df51eea44de
HMJda <hmj4669@gmail.com>
2022-12-02 19:53:09 +0900

    로그아웃 버튼 구현

--- TP/setting.Designer.cs
+++ TP/setting.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "로그아웃";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 56);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "회원정보";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // setting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(233, 185);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "setting";
             this.Text = "setting";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
