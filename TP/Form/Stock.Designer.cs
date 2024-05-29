c86389509ee797d838f4242d09d72ae5dd968dbc
bsh2389 <132681604+bsh2389@users.noreply.github.com>
2024-05-30 00:07:47 +0900

    test123

--- TP/Form/Stock.Designer.cs
+++ TP/Form/Stock.Designer.cs
@@ -213,7 +213,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(810, 87);
+            this.button1.Location = new System.Drawing.Point(810, 114);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(85, 37);
             this.button1.TabIndex = 15;
